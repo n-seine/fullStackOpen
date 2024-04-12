e1fa600afd45d3eaaf43aae37d78fe81f62599e0
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-04-12 23:41:41 +0200

    finish 1.6 to 1.10 (had already extracted some components before)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,11 +4,39 @@ const RateButton = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text} </button>;
 };
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <li>
+      {text} {value}
+    </li>
+  );
+};
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <ul>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="% of positive values" value={positive} />
+    </ul>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = total !== 0 ? ((good - bad) / total).toFixed(2) : undefined;
+  const positive =
+    total !== 0 ? ((good / total) * 100).toFixed(2) + " %" : undefined;
 
   return (
     <div>
@@ -22,16 +50,14 @@ const App = () => {
         <RateButton handleClick={() => setBad(bad + 1)} text="bad" />
       </section>
       <section>
-        <h1>statistics</h1>
-        {good + neutral + bad === 0 ? (
-          <p>Click a button to give feedback</p>
-        ) : (
-          <ul>
-            <li>good :{good}</li>
-            <li>neutral :{neutral}</li>
-            <li>bad :{bad}</li>
-          </ul>
-        )}
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          average={average}
+          positive={positive}
+        />
       </section>
     </div>
   );
