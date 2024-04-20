e2257f2246b20afa167a18684b45199094f6ad19
Nicolas Seine <contact@nicolasseine.com>
2024-04-20 13:03:32 +0200

    create test env

--- part4/blogList/backend/utils/config.js
+++ part4/blogList/backend/utils/config.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
-const mongoUrl = process.env.MONGO_URL;
+const mongoUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.MONGO_TEST_URL
+    : process.env.MONGO_PROD_URL;
 module.exports = { PORT, mongoUrl };
